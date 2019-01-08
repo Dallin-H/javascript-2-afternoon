f6e6538a0eeecc8408d7d2c5860a7f7dae38991d
Dallin-H <dallinhyde@gmail.com>
2019-01-08 14:28:11 -0700

    answered questions

--- practice.js
+++ practice.js
@@ -14,8 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: 'Dallin',
+  age: 26
+}
 
+me.name;
 
 
 ////////// PROBLEM 2 //////////
@@ -25,24 +29,30 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
-
+let favoriteThings = {
+  band: 'ratat',
+  food: 'italian',
+  person: 'wife',
+  book: 'Jack Reacher',
+  movie: 'Aquaman',
+  holiday: 'Christmas'
+}
 
 
 /*
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
-
+favoriteThings.car = 'Hot Wheels';
+favoriteThings.brand = 'Ford';
 
 
 /*
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
-
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 
 ////////// PROBLEM 3 //////////
@@ -55,7 +65,14 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+let backPack = {}
+
+let item = 'firstPocket';
+
+backPack[item] = 'chapstick';
+
+backPack.color = 'Khaki';
+
 
 
 
@@ -63,8 +80,7 @@
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
-
+backPack;
 
 
 /*
@@ -73,8 +89,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
-
+console.log(backPack);
 
 
 ////////// PROBLEM 4 //////////
@@ -96,8 +111,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
